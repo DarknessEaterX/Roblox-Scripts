74e5bc768938a73a42e8fae0c036d19734ab9cc2
DarknessEaterX <nolant736@gmail.com>
2025-05-18 10:26:34 +0200

    Update Notification2.lua

--- Notification2.lua
+++ Notification2.lua
@@ -1,1 +1,33 @@
--- UI Backpack v1
+-- Notification Module
+
+local NotificationService = {}
+
+-- Configuration
+local animationDuration = 0.3 -- Seconds
+local notificationLifetime = 5 -- Seconds
+local notificationSpacing = 5 -- Pixels
+local safeAreaOffset = 10 -- Pixels
+
+-- UI Elements
+local screenGui
+local notificationContainer
+
+-- Callbacks
+local onDismissCallback
+
+-- Helper Functions
+local function Create(objectType, properties)
+    local object = Instance.new(objectType)
+    for property, value in pairs(properties) do
+        object[property] = value
+    end
+    return object
+end
+
+local function Animate(guiObject, property, startValue, endValue, duration)
+    local tweenInfo = TweenInfo.new(duration, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
+    local tween = game:GetService("TweenService"):Create(guiObject, tweenInfo, {[property] = endValue})
+    tween:Play()
+    return tween
+end
+
